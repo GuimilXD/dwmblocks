f23884725545304ca041797a202ef223f8589082
Guilherme <tristaoguilherme13@gmail.com>
2021-01-28 13:45:50 -0300

    config

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "scripts/dwmupdates.sh", 30, 		0}, 
-	{"", "scripts/dwmcpu.sh", 30, 		0}, 
-	{"", "scripts/dwmvolume.sh", 0, 		0}, 
-	{"", "scripts/dwmweather.sh", 18000, 		0}, 
-	{"", "scripts/dwmclock.sh",	5,		0},
+	{"", "dwmupdates.sh", 30, 		0}, 
+	{"", "dwmcpu.sh", 30, 		0}, 
+	{"", "dwmvolume.sh", 0, 		0}, 
+	{"", "dwmweather.sh", 18000, 		0}, 
+	{"", "dwmclock.sh",	5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

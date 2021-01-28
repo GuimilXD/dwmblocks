6132571b820683123eb1f8db031b3cf49293280b
Guilherme <tristaoguilherme13@gmail.com>
2021-01-28 19:38:18 -0300

    config

--- scripts/dwmvolume.sh
+++ scripts/dwmvolume.sh
@@ -4,12 +4,3 @@ volume="$(pactl list sinks | grep '^[[:space:]]Volume:' | \
     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
 icon=""
 printf " %s  %s" "$icon" "$volume%"
-
-
-case $BLOCK_BUTTON in
-    1) echo "Please work =(" ;;
-    2) echo "Please work =(" ;;
-    3) echo "Please work =(" ;;
-    4) echo "Please work =(" ;;
-    5) echo "Please work =(" ;;
-esac
